78c9c7064c9e91d70202401c5667f78293e82e81
lobapirigotica <54606943+lobapirigotica@users.noreply.github.com>
2020-05-25 21:35:01 -0300

    Add files via upload

---
+++ email.php
@@ -0,0 +1,45 @@
+<?php
+
+$GetParam = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+$Nome = $GetParam['nome'];
+$Email = $GetParam['email'];
+$Mensagem = "https://danielaseolino.000webhostapp.com/capturaemail/arquivo/saudavel.pdf";
+
+$Email_remetente = "daniela-seolino@educar.rs.gov.br";
+$Email_destinatario = "{$Email}";
+$Email_reply = "dseolino@gmail.com";
+
+$Email_assunto = "Novo contato pelo site";
+$Email_conteudo = "Nome = {$Nome} \n";
+$Email_conteudo .="Email = {$Email} \n";
+$Email_conteudo .="Mensagem = {$Mensagem} \n";
+$HeaderArray = array("From: $Email_remetente",
+    "Reply-To: $Email_reply",
+    "Subect: $Email_assunto",
+    "Return-Path: $Email_remetente",
+    "MIME-Version: 1.0",
+    "X-Priority: 3",
+    "Content-Type: text/html; charset=UTF-8");
+$Email_headers = implode("\n", $HeaderArray);
+
+if(mail($Email_destinatario, $Email_assunto, $Email_conteudo, $Email_headers)){
+    
+    ?>
+    
+    <div class="trigger trigger-sucess">
+        <p>SUCESSO: O arquivo foi enviado ao seu email com sucesso!</p>
+    </div>
+    <?php
+} else{
+    ?>
+    <div class="trigger trigger-error">
+        <p>FALHA: Tente novamente!</p>
+    </div>
+    <?php
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./css/style.css" media="screen" />
+    <title>captura de email</title>
+</head>
+<body>
+
+    <div id="faixa"></div>
+    <div id="titulo">Faça seu cadastro agora mesmo!</div>
+    <div id="texto">Finalize seu cadastro agora mesmo e ganhe um acesso as melhores dicas e receitas para você ter uma vida mais saudável e com menos medidas. Não perca essa oportunidade!</div>
+    <div id="tabela">
+        <form  method="post" action="email.php">
+            <div id="titulo_formulario">Fazer login</div>
+            <div id="barra_nome">
+                <input id="barra_email" name="email" type="email" placeholder="email">
+            </div>
+            <div id="barra_senha_externa">
+                <input id="barra_senha" name="nome" type="name" placeholder="nome">
+            </div>
+            <div id="barra_enviar_externo">        
+                <input id="barra_enviar" name="submit" type="submit" formmethod="POST">
+            </div>
+                
+        </form>
+    </div>
+    <div id="imagem_principal">
+        <img src="imagens/alimentos1.png" width=100% >
+    </div>
+    
+
+
+</body>
